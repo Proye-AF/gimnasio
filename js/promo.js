78fb624ffe678be8d7f1d06c83777227b0ec1a5c
Faliendre <favioaliendre@gmail.com>
2024-01-08 20:53:48 -0400

    pocos cambios

--- js/promo.js
+++ js/promo.js
@@ -5,7 +5,7 @@ function cargarPromo (){
     const div = document.createElement("div")
     div.classList.add("divPromo")
     div.innerHTML = `
-    <img src="./img/promo-popup.webp">
+    <img src="./img/promo-popup.webp" id="img_prom">
     <button class="btn btn-danger top" onclick="cerrarPromo()">X</button>
     <a class="banner__items__boton position" href="https://api.whatsapp.com/send/?phone=59171758007&text=Hola! Quiero solicitar mas informacion sobre el gym." target="_blank"><button>QUIERO LA PROMO</button></a>
     `
